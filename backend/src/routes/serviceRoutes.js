3a80d4e7b0b675b39e32497462b1f6c18a5cf1bf
Gabrielagomesf <goomes.016@gmail.com>
2025-12-05 01:49:44 -0300

    Fix: Corrigir binding das rotas de serviço

--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 const authenticateToken = require('../middleware/auth');
 
-router.post('/', authenticateToken, serviceController.create.bind(serviceController));
-router.get('/:id', authenticateToken, serviceController.getById.bind(serviceController));
-router.get('/client/:clientId', authenticateToken, serviceController.getByClientId.bind(serviceController));
-router.get('/professional/:professionalId', authenticateToken, serviceController.getByProfessionalId.bind(serviceController));
-router.put('/:id', authenticateToken, serviceController.update.bind(serviceController));
-router.delete('/:id', authenticateToken, serviceController.delete.bind(serviceController));
+router.post('/', authenticateToken, serviceController.create);
+router.get('/:id', authenticateToken, serviceController.getById);
+router.get('/client/:clientId', authenticateToken, serviceController.getByClientId);
+router.get('/professional/:professionalId', authenticateToken, serviceController.getByProfessionalId);
+router.put('/:id', authenticateToken, serviceController.update);
+router.delete('/:id', authenticateToken, serviceController.delete);
 
 module.exports = router;
 
